3cb35f044cc898ad04ea284be9607915f0559ad3
Joan He <johe@magento.com>
2018-04-06 11:44:46 -0500

    MAGETWO-89443: Release New Version of ZF1
    - Remove Zend_Auth Tests

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -26,8 +26,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
 }
 
 require_once 'Zend/Acl/AclTest.php';
-require_once 'Zend/AuthTest.php';
-require_once 'Zend/Auth/AllTests.php';
 require_once 'Zend/Cache/AllTests.php';
 require_once 'Zend/Db/AllTests.php';
 require_once 'Zend/ConfigTest.php';
@@ -139,8 +137,6 @@ class Zend_AllTests
 
         // Start remaining tests...
         $suite->addTestSuite('Zend_Acl_AclTest');
-        $suite->addTestSuite('Zend_AuthTest');
-        $suite->addTest(Zend_Auth_AllTests::suite());
         $suite->addTest(Zend_Cache_AllTests::suite());
         $suite->addTestSuite('Zend_ConfigTest');
         $suite->addTest(Zend_Config_AllTests::suite());
