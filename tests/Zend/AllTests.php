a62fd620c98f05d168edb3f584499e8ded2de7ba
Joan He <johe@magento.com>
2018-04-06 11:47:53 -0500

    MAGETWO-89443: Release New Version of ZF1
    - Remove Zend_Ldap Tests

--- tests/Zend/AllTests.php
+++ tests/Zend/AllTests.php
@@ -42,7 +42,6 @@ require_once 'Zend/Filter/AllTests.php';
 require_once 'Zend/Http/AllTests.php';
 require_once 'Zend/JsonTest.php';
 require_once 'Zend/Json/AllTests.php';
-require_once 'Zend/Ldap/AllTests.php';
 require_once 'Zend/LoaderTest.php';
 require_once 'Zend/Loader/AllTests.php';
 require_once 'Zend/LocaleTest.php';
@@ -149,7 +148,6 @@ class Zend_AllTests
         $suite->addTest(Zend_Http_AllTests::suite());
         $suite->addTestSuite('Zend_JsonTest');
         $suite->addTest(Zend_Json_AllTests::suite());
-        $suite->addTest(Zend_Ldap_AllTests::suite());
         $suite->addTestSuite('Zend_LoaderTest');
         $suite->addTest(Zend_Loader_AllTests::suite());
         $suite->addTestSuite('Zend_LocaleTest');
